42c1396d5e80200d0f5babf1c870c75dfb424560
Amirreza Nayyeri <ar.nayyeri99@gmail.com>
2021-02-01 16:37:26 +0330

    Final Changes

--- src/UI.java
+++ src/UI.java
@@ -1,7 +1,3 @@
-/**
- * Sample Skeleton for 'UI.fxml' Controller Class
- */
-
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -30,6 +26,9 @@ public class UI {
 
     @FXML // fx:id="result"
     private Label result; // Value injected by FXMLLoader
+
+    @FXML // fx:id="description"
+    private Label description; // Value injected by FXMLLoader
 
     @FXML
     void check(ActionEvent event) {
@@ -93,17 +92,33 @@ public class UI {
 
     @FXML
     void insert_button(ActionEvent event) {
-        tree.insertWord(word.getText(), tree.numberWords);
+        if (tree.contain(word.getText())) {
+            description.setText("Word is exist in the tree!");
+        } else if (getNonLetter(word.getText(), 0) != -1 || word.getText().equals("")) {
+            description.setText("Word is invalid!");
+        } else {
+            tree.insertWord(word.getText(), tree.numberWords);
+            description.setText("Insert Successfully");
+            result.setText("");
+        }
     }
 
     @FXML
     void remove_button(ActionEvent event) {
-        tree.removeWord(word.getText());
+        if (tree.contain(word.getText())) {
+            tree.removeWord(word.getText());
+            description.setText("Remove Successfully");
+            result.setText("");
+        } else {
+            description.setText("Word isn't exist in the tree!");
+        }
     }
 
     @FXML
     void reset(ActionEvent event) {
         tree = new TrieTree(new TrieNode());
+        description.setText("Reset Successfully");
+        result.setText("");
     }
 
     @FXML
@@ -112,6 +127,7 @@ public class UI {
         assert text != null : "fx:id=\"text\" was not injected: check your FXML file 'UI.fxml'.";
         assert word != null : "fx:id=\"insert_text\" was not injected: check your FXML file 'UI.fxml'.";
         assert result != null : "fx:id=\"result\" was not injected: check your FXML file 'UI.fxml'.";
+        assert description != null : "fx:id=\"description\" was not injected: check your FXML file 'UI.fxml'.";
         tree = new TrieTree(new TrieNode());
     }
 }
